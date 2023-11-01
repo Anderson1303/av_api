dd6cd1009a4039d3b9b270512540d8330de06c80
unknown <andersonfranz13@gmail.com>
2023-10-31 22:28:41 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -8,18 +8,16 @@ const cors = require('cors');
 
 // Initialize Express app
 app.disable('x-powered-by')
+
+let corsOptions = { 
+    origin : ['http://localhost:3002','http://localhost:3003','http://localhost:3000'], 
+} 
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }))
 app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
 
 restoreSessions()
-
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
-    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-});
 
 app.use('/', routes)
 
