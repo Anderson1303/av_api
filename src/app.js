b6515b87c96fa8e0021c804ea512cc3f3c89583d
unknown <andersonfranz13@gmail.com>
2023-10-31 21:57:57 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: tr
 
 restoreSessions()
 
-//app.use(cors());
+app.use(cors());
 
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
     res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-});*/
+});
 
 app.use('/', routes)
 
